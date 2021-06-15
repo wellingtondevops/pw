1919c28c92c451d84a476cfac1f8c4cfeb80a78a
wellingtondevops <wellingtondevops@gmail.com>
2021-06-15 15:54:49 -0300

    fix

--- push.js
+++ push.js
@@ -9,5 +9,5 @@ let vapidKeys =
 
 push.setVapidDetails('mailto:test@code.co.uk', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub ={}
+let sub = { "endpoint": "https://fcm.googleapis.com/fcm/send/d2r0-DVpK_Q:APA91bHqlop8t2UXz5zx0s71cfW7HuTA0QcLqzMfDdyMDwzZPeK8U0YxmuNLCYxFjEsulmw9v26tLoXwAfZui2Mzqv6DK9_tOyl8tW46yxJv6dqXaCuv9_qGIFxug0TH_U8_tF4QxYmw", "expirationTime": null, "keys": { "p256dh": "BIEuBC7mguIBLmrpeRq7mARpc4dZQaCIepvnxVxoxoPtlhT1zKGyk4ncvy8UlwM8qk8FyudNjdsaLjEZZHEMFVk", "auth": "Mh3ymt9wL3DDJuL-vqg47g" } }
 push.sendNotification(sub, 'teste message')
